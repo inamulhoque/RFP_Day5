d548f7bd098536aa2bb43b6bb14910b703a52370
Inamul <inamulhoque097@gmail.com>
2022-06-02 11:48:08 +0530

    Create: A two dimensional array

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 public class TwoDArray {
+    public static void twoDArr(int m, int n){
+        int arr[][] = new int[m][n];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i<m; i++){
+            for (int j=0; j<n; j++){
+                arr[i][j]= (2*i)+1;
+            }
+        }
+
+        for (int i = 0; i<m; i++){
+            for (int j=0; j<n; j++){
+                System.out.print("arr["+i+"]["+j+"]="+arr[i][j]+"\t");
+            }
+            System.out.println(" ");
+        }
+    }
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter row size:");
         int m = obj.nextInt();
         System.out.println("Enter column size:");
         int n = obj.nextInt();
-        String arr[][] = new String[m][n];
-        System.out.println("Enter array elements: ");
-        for (int i = 0; i<m; i++){
-            for (int j=0; j<n; j++){
-                arr[i][j]=obj.nextLine();
-            }
-        }
-
-        for (int i = 0; i<m; i++){
-            for (int j=0; j<n; j++){
-                System.out.print("arr["+i+"]["+j+"]="+arr[i][j]);
-                System.out.println("\t");
-            }
-            System.out.println(" ");
-        }
+        TwoDArray.twoDArr(m,n);
     }
 }
